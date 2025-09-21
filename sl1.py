fdbca4133d07ba0d47b20b8b9c8262decda935e1
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-21 16:02:43 -0500

    layout change

--- sl1.py
+++ sl1.py
@@ -1,9 +1,10 @@
 import streamlit as st
 #streamlit run sl1.py
+st.set_page_config(
 st.image("https://theworldmusicfoundation.org/wp-content/uploads/2016/11/wmf_small_logo.png.webp", width=180)
 st.title("About Us")
 st.write("This app is created by the World Music Foundation to share music and uplift local businesses.")
-
+)
 
 # Learn More
 st.markdown("---")
