a23191a2f6b438f087b40b86a5b0bcd95ebf8de2
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-21 17:16:54 -0500

    layout change

--- pages/1_about.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.set_page_config(
-    page_title="About App",
-    page_icon="🎶",
-    layout="centered",
-)
-
-
--- sl1.py
+++ sl1.py
@@ -14,20 +14,3 @@ st.markdown("---")
 with st.expander("Learn more about us and our goals"):
     st.write("https://theworldmusicfoundation.org/")
 
-#about_page = st.Page(
-    #page="pages/1_about.py",
-    #title="About Us",
-    #icon=":material/density_medium:",
-
-#)
-#drinks_page = st.Page(
-#    page="pages/2_drinks.py",
-#    title="Drinks",
-#    icon=":material/coffee:",
-#)
-
-# Navigation Setup
-#pg = st.navigation(pages=[drinks_page])
-
-# Run Navigation
-#pg.run()
