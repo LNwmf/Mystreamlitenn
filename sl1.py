c6ef7766465534d036c551fc029b50dce7a4f9a1
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-21 16:20:08 -0500

    layout change

--- sl1.py
+++ sl1.py
@@ -1,9 +1,8 @@
 import streamlit as st
 #streamlit run sl1.py
-st.set_page_config(
-    page_title="About Us",
-    layout="centered",
-)
+st.set_page_config(page_title="About Us", layout="centered")
+page_title="About Us"
+layout="centered"
 #st.image("https://theworldmusicfoundation.org/wp-content/uploads/2016/11/wmf_small_logo.png.webp", width=180)
 st.title("About Us")
 st.write("This app is created by the World Music Foundation to share music and uplift local businesses.")
