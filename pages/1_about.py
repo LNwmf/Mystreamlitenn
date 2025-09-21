0754450188fba53d7078e11a650746380964d2e7
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-21 16:50:42 -0500

    layout change

--- pages/1_about.py
+++ pages/1_about.py
@@ -1,4 +1,8 @@
 import streamlit as st
-
+st.set_page_config(
+    page_title="About App",
+    page_icon="🎶",
+    layout="centered",
+)
 
 
--- sl1.py
+++ sl1.py
@@ -20,14 +20,14 @@ with st.expander("Learn more about us and our goals"):
     #icon=":material/density_medium:",
 
 #)
-drinks_page = st.Page(
-    page="pages/2_drinks.py",
-    title="Drinks",
-    icon=":material/coffee:",
-)
+#drinks_page = st.Page(
+#    page="pages/2_drinks.py",
+#    title="Drinks",
+#    icon=":material/coffee:",
+#)
 
 # Navigation Setup
-pg = st.navigation(pages=[drinks_page])
+#pg = st.navigation(pages=[drinks_page])
 
 # Run Navigation
 #pg.run()
