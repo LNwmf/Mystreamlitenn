129ec62a13ec7bbedcbef0d06fd6fb216c05b133
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-21 14:57:30 -0500

    layout change

--- views/1_about.py
+++ views/1_about.py
@@ -1,3 +1,4 @@
 import streamlit as st
-
-st.title("About Us")
+def show():
+    st.title("About Us")
+    st.write("This app is created by the World Music Foundation to share music and uplift local businesses.")
